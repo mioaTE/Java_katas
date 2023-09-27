974c3ade2e763fae25b8f6e79052651a5813a442
Mio Arai <mio311a@gmail.com>
2023-09-27 14:24:28 -0400

    factorial

--- src/Main.java
+++ src/Main.java
@@ -26,5 +26,20 @@ public class Main {
             }
             return isIncreasing;
         }
+
+//        Create a method called factorial that takes in an integer n and returns the factorial of the number. A factorial is the product of all positive integers less than or equal to n.
+//
+//        For example:
+//
+//        factorial(3) → 6 (since 1 * 2 * 3 = 6)
+//        factorial(4) → 24 (since 1 * 2 * 3 * 4 = 24)
+//        factorial(10) → 3628800
+        public int factorial(int n){
+            int result = 1;
+            for(int i = 1; i <= n; i++){
+                result *= i;
+            }
+            return result;
+        }
     }
 }
